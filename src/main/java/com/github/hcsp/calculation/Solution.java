daa23fcae960174c490a48ea5451942fcfeb3396
Chen <mforward@126.com>
2022-08-07 18:35:26 +0800

    wwwwww

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
     public static void printNumberTo10(int number) {
         while (true) {
             System.out.println(number);
-            ++number;
+            number++;
             if (number > 10) {
                 return;
             }
